59dab270903883b749cb52b68c292e94fbd93361
Rajkumar Nayak <rajkumarnayak20@gmail.com>
2026-04-03 14:11:14 +0530

    dfg

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,96 +1,191 @@
-// import React, { useEffect } from 'react'
-// import './App.css'
-// import { Outlet } from 'react-router-dom'
-// import Header from './components/Header'
-// import Footer from './components/Footer'
-// import toast, { Toaster } from 'react-hot-toast';
-// import fetchUserDetails from './utils/fetchUserDetails'
+// // import React, { useEffect } from 'react'
+// // import './App.css'
+// // import { Outlet } from 'react-router-dom'
+// // import Header from './components/Header'
+// // import Footer from './components/Footer'
+// // import toast, { Toaster } from 'react-hot-toast';
+// // import fetchUserDetails from './utils/fetchUserDetails'
 
-// function App () {
-//   const fetchUser =async ()=>{
-//     const userData = await fetchUserDetails()
-//   console.log("user data ", userData);
-//   }
+// // function App () {
+// //   const fetchUser =async ()=>{
+// //     const userData = await fetchUserDetails()
+// //   console.log("user data ", userData);
+// //   }
+
+// //   useEffect(() => {
+// //     fetchUser()
+// //   },[]
+// // )
+
+// //   return (
+// //   <>
+// //    <Header/>
+// //     <main className='min-h-[78vh]'>
+// //       <Outlet/>
+
+// //     </main>
+// //     <Footer/>
+// //     <Toaster/>
+// //   </>
+// //   )
+// // }
+
+// // export default App
+
+// import React, { use, useEffect } from "react";
+// import "./App.css";
+// import { Outlet, useLocation } from "react-router-dom";
+// import Header from "./components/Header";
+// import Footer from "./components/Footer";
+// import { Toaster } from "react-hot-toast";
+// import fetchUserDetails from "./utils/fetchUserDetails";
+
+// import { setUserDetails } from "./store/userSlice";
+// import userReducer from "./store/userSlice";
+// import {
+//   setAllCategory,
+//   setAllSubCategory,
+//   setLoadingCategory,
+// } from "./store/productSlice";
+// import { useDispatch } from "react-redux";
+// import { fetchCategory, fetchSubCategory } from "./utils/fetchCategory"; // Import from new file
+
+// import GlobalProvider from "./provider/GlobalProvider.jsX";
+// import { FaShoppingCart } from "react-icons/fa";
+// import { useGlobalContext } from "./provider/GlobalProvider";
+// import Cartmobilelink from "./components/cartmobile";
+
+// function App() {
+//   const dispatch = useDispatch();
+//   const location =useLocation()
+ 
+ 
+//   const fetchUser = async () => {
+//     const userData = await fetchUserDetails();
+//     dispatch(setUserDetails(userData.data));
+//   };
 
 //   useEffect(() => {
-//     fetchUser()
-//   },[]
-// )
+//     fetchUser();
+//     fetchCategory(dispatch, setLoadingCategory, setAllCategory);
+//     fetchSubCategory(dispatch, setLoadingCategory, setAllSubCategory);
+//     // fetchCartItem()
+//   }, []);
+
 
 //   return (
-//   <>
-//    <Header/>
-//     <main className='min-h-[78vh]'>
-//       <Outlet/>
+//     <GlobalProvider>
+//       <Header />
+//       <main className="min-h-[78vh]">
+//         <Outlet />
+//       </main>
+//       <Footer />
+//       <Toaster />
+//       {
+// location.pathname !== 'checkout' && (
 
-//     </main>
-//     <Footer/>
-//     <Toaster/>
-//   </>
-//   )
-// }
-
-// export default App
-
-import React, { use, useEffect } from "react";
-import "./App.css";
-import { Outlet, useLocation } from "react-router-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import { Toaster } from "react-hot-toast";
-import fetchUserDetails from "./utils/fetchUserDetails";
-
-import { setUserDetails } from "./store/userSlice";
-
-import {
-  setAllCategory,
-  setAllSubCategory,
-  setLoadingCategory,
-} from "./store/productSlice";
-import { useDispatch } from "react-redux";
-import { fetchCategory, fetchSubCategory } from "./utils/fetchCategory"; // Import from new file
-
-import GlobalProvider from "./provider/GlobalProvider.jsX";
-import { FaShoppingCart } from "react-icons/fa";
-import { useGlobalContext } from "./provider/GlobalProvider";
-import Cartmobilelink from "./components/cartmobile";
-
-function App() {
-  const dispatch = useDispatch();
-  const location =useLocation()
- 
- 
-  const fetchUser = async () => {
-    const userData = await fetchUserDetails();
-    dispatch(setUserDetails(userData.data));
-  };
-
-  useEffect(() => {
-    fetchUser();
-    fetchCategory(dispatch, setLoadingCategory, setAllCategory);
-    fetchSubCategory(dispatch, setLoadingCategory, setAllSubCategory);
-    // fetchCartItem()
-  }, []);
-
-
-  return (
-    <GlobalProvider>
-      <Header />
-      <main className="min-h-[78vh]">
-        <Outlet />
-      </main>
-      <Footer />
-      <Toaster />
-      {
-location.pathname !== 'checkout' && (
-
-  <Cartmobilelink/>
-)
-      }
+//   <Cartmobilelink/>
+// )
+//       }
 
      
+//     </GlobalProvider>
+//   );
+// }
+
+
+
+
+// export default App;
+
+
+
+import React from 'react'
+import { Outlet, useLocation } from 'react-router-dom'
+import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import toast, { Toaster } from 'react-hot-toast';
+import { useEffect } from 'react';
+import fetchUserDetails from './utils/fetchUserDetails';
+import { setUserDetails } from './store/userSlice';
+import { setAllCategory,setAllSubCategory,setLoadingCategory } from './store/productSlice';
+import { useDispatch } from 'react-redux';
+import Axios from './utils/Axios';
+import SummaryApi from './common/SummaryApi';
+import { handleAddItemCart } from './store/cartProduct'
+import GlobalProvider from './provider/GlobalProvider';
+import { FaCartShopping } from "react-icons/fa6";
+import CartMobileLink from './components/CartMobile';
+
+function App() {
+  const dispatch = useDispatch()
+  const location = useLocation()
+  
+
+  const fetchUser = async()=>{
+      const userData = await fetchUserDetails()
+      dispatch(setUserDetails(userData.data))
+  }
+
+  const fetchCategory = async()=>{
+    try {
+        dispatch(setLoadingCategory(true))
+        const response = await Axios({
+            ...SummaryApi.getCategory
+        })
+        const { data : responseData } = response
+
+        if(responseData.success){
+           dispatch(setAllCategory(responseData.data.sort((a, b) => a.name.localeCompare(b.name)))) 
+        }
+    } catch (error) {
+        
+    }finally{
+      dispatch(setLoadingCategory(false))
+    }
+  }
+
+  const fetchSubCategory = async()=>{
+    try {
+        const response = await Axios({
+            ...SummaryApi.getSubCategory
+        })
+        const { data : responseData } = response
+
+        if(responseData.success){
+           dispatch(setAllSubCategory(responseData.data.sort((a, b) => a.name.localeCompare(b.name)))) 
+        }
+    } catch (error) {
+        
+    }finally{
+    }
+  }
+
+  
+
+  useEffect(()=>{
+    fetchUser()
+    fetchCategory()
+    fetchSubCategory()
+    // fetchCartItem()
+  },[])
+
+  return (
+    <GlobalProvider> 
+      <Header/>
+      <main className='min-h-[78vh]'>
+          <Outlet/>
+      </main>
+      <Footer/>
+      <Toaster/>
+      {
+        location.pathname !== '/checkout' && (
+          <CartMobileLink/>
+        )
+      }
     </GlobalProvider>
-  );
+  )
 }
 
-export default App;
+export default App

319f79bbfb182f2654e700e27a564cf3e3d81eaf
Rajkumar Nayak <rajkumarnayak20@gmail.com>
2026-04-09 14:50:39 +0530

    gfd

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -30,11 +30,11 @@
 
 import { configureStore } from "@reduxjs/toolkit";
 
-import userReducer from "../userSlice"
-import productReducer from "../productSlice";
-import addressReducer from "../addressSlice";
-import cartReducer from "../cartSlice"
-import orderReducer from "../orderSlice";
+import userReducer from "./ userSlice"
+import productReducer from "./productSlice";
+import addressReducer from "./addressSlice";
+import cartReducer from "./cartSlice"
+import orderReducer from "./orderSlice";
 
 export const store = configureStore({
   reducer: {

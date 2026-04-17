acdfb5f187fa2831a13bcef5d6b440c69fb33844
Rajkumar Nayak <rajkumarnayak20@gmail.com>
2026-04-17 16:03:10 +0530

    tt

--- client/src/utils/Axios.js
+++ client/src/utils/Axios.js
@@ -1,157 +1,74 @@
-// // // import axios from "axios";
-// // // import SummaryApi , { baseURL } from "../common/SummaryApi";
+// // import axios from "axios";
+// // import SummaryApi , { baseURL } from "../common/SummaryApi";
 
-// // // const Axios = axios.create({
-// // //     baseURL : baseURL,
-// // //     withCredentials : true
-// // // })
+// // const Axios = axios.create({
+// //     baseURL : baseURL,
+// //     withCredentials : true
+// // })
 
-// // // //sending access token in the header
-// // // Axios.interceptors.request.use(
-// // //     async(config)=>{
-// // //         const accessToken = localStorage.getItem('accesstoken')
+// // //sending access token in the header
+// // Axios.interceptors.request.use(
+// //     async(config)=>{
+// //         const accessToken = localStorage.getItem('accesstoken')
 
-// // //         if(accessToken){
-// // //             config.headers.Authorization = `Bearer ${accessToken}`
-// // //         }
+// //         if(accessToken){
+// //             config.headers.Authorization = `Bearer ${accessToken}`
+// //         }
 
-// // //         return config
-// // //     },
-// // //     (error)=>{
-// // //         return Promise.reject(error)
-// // //     }
-// // // )
+// //         return config
+// //     },
+// //     (error)=>{
+// //         return Promise.reject(error)
+// //     }
+// // )
 
-// // // //extend the life span of access token with 
-// // // // the help refresh
-// // // Axios.interceptors.request.use(
-// // //     (response)=>{
-// // //         return response
-// // //     },
-// // //     async(error)=>{
-// // //         let originRequest = error.config 
+// // //extend the life span of access token with 
+// // // the help refresh
+// // Axios.interceptors.request.use(
+// //     (response)=>{
+// //         return response
+// //     },
+// //     async(error)=>{
+// //         let originRequest = error.config 
 
-// // //         if(error.response.status === 401 && !originRequest.retry){
-// // //             originRequest.retry = true
+// //         if(error.response.status === 401 && !originRequest.retry){
+// //             originRequest.retry = true
 
-// // //             const refreshToken = localStorage.getItem("refreshToken")
+// //             const refreshToken = localStorage.getItem("refreshToken")
 
-// // //             if(refreshToken){
-// // //                 const newAccessToken = await refreshAccessToken(refreshToken)
+// //             if(refreshToken){
+// //                 const newAccessToken = await refreshAccessToken(refreshToken)
 
-// // //                 if(newAccessToken){
-// // //                     originRequest.headers.Authorization = `Bearer ${newAccessToken}`
-// // //                     return Axios(originRequest)
-// // //                 }
-// // //             }
-// // //         }
+// //                 if(newAccessToken){
+// //                     originRequest.headers.Authorization = `Bearer ${newAccessToken}`
+// //                     return Axios(originRequest)
+// //                 }
+// //             }
+// //         }
         
-// // //         return Promise.reject(error)
-// // //     }
-// // // )
+// //         return Promise.reject(error)
+// //     }
+// // )
 
 
-// // // const refreshAccessToken = async(refreshToken)=>{
-// // //     try {
-// // //         const response = await Axios({
-// // //             ...SummaryApi.refreshToken,
-// // //             headers : {
-// // //                 Authorization : `Bearer ${refreshToken}`
-// // //             }
-// // //         })
+// // const refreshAccessToken = async(refreshToken)=>{
+// //     try {
+// //         const response = await Axios({
+// //             ...SummaryApi.refreshToken,
+// //             headers : {
+// //                 Authorization : `Bearer ${refreshToken}`
+// //             }
+// //         })
 
-// // //         const accessToken = response.data.data.accessToken
-// // //         localStorage.setItem('accesstoken',accessToken)
-// // //         return accessToken
-// // //     } catch (error) {
-// // //         console.log(error)
-// // //     }
-// // // }
+// //         const accessToken = response.data.data.accessToken
+// //         localStorage.setItem('accesstoken',accessToken)
+// //         return accessToken
+// //     } catch (error) {
+// //         console.log(error)
+// //     }
+// // }
 
-// // // export default Axios//6:00
-
-
-
-
-
-
-// // // import axios from "axios";
-// // // import SummaryApi, { baseURL } from "../common/SummaryApi";
-
-// // // const Axios = axios.create({
-// // //   baseURL: baseURL,
-// // //   withCredentials: true
-// // // });
-
-// // // //  REQUEST INTERCEPTOR (attach access token)
-// // // Axios.interceptors.request.use(
-// // //   (config) => {
-// // //     const accessToken = localStorage.getItem("accesstoken");
-
-// // //     if (accessToken) {
-// // //       config.headers.Authorization = `Bearer ${accessToken}`;
-// // //     }
-
-// // //     return config;
-// // //   },
-// // //   (error) => Promise.reject(error)
-// // // );
-
-// // // //  RESPONSE INTERCEPTOR (refresh token logic)
-// // // Axios.interceptors.response.use(
-// // //   (response) => {
-// // //     return response;
-// // //   },
-// // //   async (error) => {
-// // //     const originRequest = error.config;
-
-// // //     if (error.response?.status === 401 && !originRequest._retry) {
-// // //       originRequest._retry = true;
-
-// // //       const refreshToken = localStorage.getItem("refreshToken");
-
-// // //       if (refreshToken) {
-// // //         try {
-// // //           const newAccessToken = await refreshAccessToken(refreshToken);
-
-// // //           if (newAccessToken) {
-// // //             originRequest.headers.Authorization = `Bearer ${newAccessToken}`;
-// // //             return Axios(originRequest);
-// // //           }
-// // //         } catch (err) {
-// // //           console.log("Refresh token failed:", err);
-// // //         }
-// // //       }
-// // //     }
-
-// // //     return Promise.reject(error);
-// // //   }
-// // // );
-
-// // // // ✅ REFRESH TOKEN FUNCTION
-// // // const refreshAccessToken = async (refreshToken) => {
-// // //   try {
-// // //     const response = await Axios({
-// // //       ...SummaryApi.refreshToken,
-// // //       headers: {
-// // //         Authorization: `Bearer ${refreshToken}`
-// // //       }
-// // //     });
-
-// // //     // const accessToken = response.data.data.accessToken;
-// // //     const accessToken = response.data.data.accesstoken
-
-// // //     localStorage.setItem("accesstoken", accessToken);
-
-// // //     return accessToken;
-
-// // //   } catch (error) {
-// // //     console.log("Refresh error:", error);
-// // //     return null;
-// // //   }
-// // // };
-
-// // // export default Axios;
+// // export default Axios//6:00
 
 
 
@@ -166,35 +83,44 @@
 // //   withCredentials: true
 // // });
 
-// // // ✅ Attach access token
+// // //  REQUEST INTERCEPTOR (attach access token)
 // // Axios.interceptors.request.use(
 // //   (config) => {
-// //     const token = localStorage.getItem("accesstoken");
+// //     const accessToken = localStorage.getItem("accesstoken");
 
-// //     if (token) {
-// //       config.headers.Authorization = `Bearer ${token}`;
+// //     if (accessToken) {
+// //       config.headers.Authorization = `Bearer ${accessToken}`;
 // //     }
 
 // //     return config;
-// //   }
+// //   },
+// //   (error) => Promise.reject(error)
 // // );
 
-// // // ✅ Separate instance (IMPORTANT)
-// // const refreshAxios = axios.create({
-// //   baseURL: baseURL,
-// //   withCredentials: true
-// // });
+// // //  RESPONSE INTERCEPTOR (refresh token logic)
+// // Axios.interceptors.response.use(
+// //   (response) => {
+// //     return response;
+// //   },
+// //   async (error) => {
+// //     const originRequest = error.config;
 
-// // // ✅ Refresh function
-// // const refreshAccessToken = async () => {
-// //   try {
-// //     const response = await refreshAxios({
-// //       ...SummaryApi.refreshToken
-// //     });
+// //     if (error.response?.status === 401 && !originRequest._retry) {
+// //       originRequest._retry = true;
 
-// //       if (newToken) {
-// //         originalRequest.headers.Authorization = `Bearer ${newToken}`;
-// //         return Axios(originalRequest);
+// //       const refreshToken = localStorage.getItem("refreshToken");
+
+// //       if (refreshToken) {
+// //         try {
+// //           const newAccessToken = await refreshAccessToken(refreshToken);
+
+// //           if (newAccessToken) {
+// //             originRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+// //             return Axios(originRequest);
+// //           }
+// //         } catch (err) {
+// //           console.log("Refresh token failed:", err);
+// //         }
 // //       }
 // //     }
 
@@ -202,59 +128,81 @@
 // //   }
 // // );
 
+// // // ✅ REFRESH TOKEN FUNCTION
+// // const refreshAccessToken = async (refreshToken) => {
+// //   try {
+// //     const response = await Axios({
+// //       ...SummaryApi.refreshToken,
+// //       headers: {
+// //         Authorization: `Bearer ${refreshToken}`
+// //       }
+// //     });
 
+// //     // const accessToken = response.data.data.accessToken;
+// //     const accessToken = response.data.data.accesstoken
+
+// //     localStorage.setItem("accesstoken", accessToken);
+
+// //     return accessToken;
+
+// //   } catch (error) {
+// //     console.log("Refresh error:", error);
+// //     return null;
+// //   }
+// // };
 
 // // export default Axios;
+
+
+
 
 
 
 // import axios from "axios";
 // import SummaryApi, { baseURL } from "../common/SummaryApi";
 
-// // ✅ Main Axios
 // const Axios = axios.create({
 //   baseURL: baseURL,
-//   withCredentials: true   // 🔥 must for cookies
+//   withCredentials: true
 // });
 
-// // ✅ Separate instance (no interceptor loop)
+// // ✅ Attach access token
+// Axios.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem("accesstoken");
+
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+
+//     return config;
+//   }
+// );
+
+// // ✅ Separate instance (IMPORTANT)
 // const refreshAxios = axios.create({
 //   baseURL: baseURL,
 //   withCredentials: true
 // });
 
-// // Refresh token function (cookie-based)
+// // ✅ Refresh function
 // const refreshAccessToken = async () => {
 //   try {
-//     await refreshAxios({
+//     const response = await refreshAxios({
 //       ...SummaryApi.refreshToken
 //     });
 
-//     return true; // success
-//   } catch (error) {
-//     return false;
-//   }
-// };
-
-// // Response interceptor (auto refresh)
-// Axios.interceptors.response.use(
-//   (response) => response,
-//   async (error) => {
-//     const originalRequest = error.config;
-
-//     if (error.response?.status === 401 && !originalRequest._retry) {
-//       originalRequest._retry = true;
-
-//       const success = await refreshAccessToken();
-
-//       if (success) {
-//         return Axios(originalRequest); // retry request
+//       if (newToken) {
+//         originalRequest.headers.Authorization = `Bearer ${newToken}`;
+//         return Axios(originalRequest);
 //       }
 //     }
 
 //     return Promise.reject(error);
 //   }
 // );
+
+
 
 // export default Axios;
 
@@ -263,87 +211,45 @@
 import axios from "axios";
 import SummaryApi, { baseURL } from "../common/SummaryApi";
 
-/* ===============================
-   ✅ MAIN AXIOS
-=============================== */
+// ✅ Main Axios
 const Axios = axios.create({
   baseURL: baseURL,
-  withCredentials: true,
+  withCredentials: true   // 🔥 must for cookies
 });
 
-/* ===============================
-   🔥 REQUEST INTERCEPTOR (IMPORTANT)
-=============================== */
-Axios.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("accessToken"); 
-      console.log("AXIOS TOKEN:", token); // 🔥 IMPORTANT
-// ✅ correct key
-
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
-
-/* ===============================
-   🔁 REFRESH AXIOS
-=============================== */
+// ✅ Separate instance (no interceptor loop)
 const refreshAxios = axios.create({
   baseURL: baseURL,
-  withCredentials: true,
+  withCredentials: true
 });
 
-/* ===============================
-   🔄 REFRESH TOKEN FUNCTION
-=============================== */
+// Refresh token function (cookie-based)
 const refreshAccessToken = async () => {
   try {
-    const response = await refreshAxios({
-      ...SummaryApi.refreshToken,
+    await refreshAxios({
+      ...SummaryApi.refreshToken
     });
 
-    // 👇 NEW ACCESS TOKEN backend thi levu
-    // const newToken = response.data?.accessToken;
-    const newToken = response.data?.data?.accessToken
-
-    if (newToken) {
-      localStorage.setItem("accessToken", newToken);
-      return newToken;
-    }
-
-    return null;
+    return true; // success
   } catch (error) {
-    return null;
+    return false;
   }
 };
 
-/* ===============================
-   🔥 RESPONSE INTERCEPTOR
-=============================== */
+// Response interceptor (auto refresh)
 Axios.interceptors.response.use(
   (response) => response,
-
   async (error) => {
     const originalRequest = error.config;
 
     if (error.response?.status === 401 && !originalRequest._retry) {
       originalRequest._retry = true;
 
-      const newToken = await refreshAccessToken();
+      const success = await refreshAccessToken();
 
-      if (newToken) {
-        // 🔥 attach new token
-        originalRequest.headers.Authorization = `Bearer ${newToken}`;
-        return Axios(originalRequest);
+      if (success) {
+        return Axios(originalRequest); // retry request
       }
-
-      // ❌ logout fallback
-      localStorage.removeItem("accessToken");
-      window.location.href = "/login";
     }
 
     return Promise.reject(error);
